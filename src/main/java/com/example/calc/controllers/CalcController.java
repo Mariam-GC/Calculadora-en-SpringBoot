33b2aedb0daa962ba94a45c9541a0c9e374cdc57
Mariam-GC <121530920+Mariam-GC@users.noreply.github.com>
2026-02-05 21:54:35 -0600

    Add RequestParam and RequestBody

--- src/main/java/com/example/calc/controllers/CalcController.java
+++ src/main/java/com/example/calc/controllers/CalcController.java
@@ -1,20 +1,37 @@
 package com.example.calc.controllers;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
 public class CalcController {
+    //Mostrar el reultado con PathVariable
     @GetMapping("/calculator/{operation}/{num1}/{num2}")
     public String calcPath(@PathVariable String operation,
                            @PathVariable double num1,
                            @PathVariable double num2) {
         double result = calculate(operation, num1, num2);
-        return "El resultado de tu suma es: " + result + ",listo, ahora pongame 10 profe";
+        return "El resultado de tu suma es: " + result + " usando PathVariable";
     }
-    
+    //Mostrar el reultado con RequestParam
+    @GetMapping("/calculatorR")
+    public String calcQuery(@RequestParam String operation,
+                            @RequestParam double num1,
+                            @RequestParam double num2) {
+        double result = calculate(operation, num1, num2  );
+        return "El resultado de tu suma es: " + result + "Usando RequestParam";
+    }
+    //Mostrar el reultado con RequestBody
+    @PostMapping("/calculatorbody")
+    public String calcBody(@RequestBody Map<String, Object> payload) {
+        String operation = (String) payload.get("operation");
+        double num1 = Double.parseDouble(payload.get("num1").toString());
+        double num2 = Double.parseDouble(payload.get("num2").toString());
 
+        double result = calculate(operation, num1, num2);
+        return "El resultado de tu suma es: " + result + "Usando RequestBody";
+    }
     public double calculate(String operation, double num1, double num2){
 
         return switch (operation){
